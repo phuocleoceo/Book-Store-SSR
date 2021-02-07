a047a31eeb8c7fedc1938b4bda785c4bfac8a1d4
phuocleoceo <ht10082001@gmail.com>
2021-02-07 21:45:45 +0700

    Register Init

--- Book-Store/Areas/Customer/Controllers/HomeController.cs
+++ Book-Store/Areas/Customer/Controllers/HomeController.cs
@@ -29,11 +29,6 @@ namespace Book_Store.Areas.Customer.Controllers
             return View(productList);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
